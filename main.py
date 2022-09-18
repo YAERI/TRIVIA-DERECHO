4ed663b20eb267994ae0227eb5fac062889a3378
70780117 <113320958+YAERI@users.noreply.github.com>
2022-09-18 15:32:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+input ("Bienvenido al Mundo del Derecho:")
+input('')
+input("¿Cuál es tu nombre?")
+input('')
+nombre = input ("")
+input('')
+print("¡Hola " + nombre + "!")
+input('')
+input('Es un placer tenerte aquí y saber que tienes el interes de conocerLos Derechos Constitucionales del Perú')
+input('')
+input('Para ello, responde las siguientes preguntas, escribiendo la letraminúscula de la alternativa que usted cree que es la correcta ¡EMPECEMOS!')
+input('')
+input('1. ¿La Constitución Política que rige actualmente en el Perú es del año?')
+input('')
+input('a)1996')
+input('b)1921')
+input('c)1983')
+input('d)1993')
+input('')
+respuesta_1 = input("Respuesta:  ")
+  
+if respuesta_1 != 'd' and respuesta_1 != 'D':
+  print('Respuesta Incorrecta')
+else:
+  print('Respuesta Correcta')
+input('')
+input('2. ¿Cuantas garantias Constitucionales hay en el Perú?')
+input('')
+input('a)6')
+input('b)5')
+input('c)8')
+input('d)3')
+input('')
+respuesta_2 = input("Respuesta:  ")
+
+if respuesta_2 != 'a' and respuesta_2 != 'A':
+  print('Respuesta Incorrecta')
+else:
+  print('Respuesta Correcta')
+input('')
+input('3. ¿El peruano tiene derecho a la libertad de creación intelectual, artística, técnica y científica, así como a la propiedad sobre dichas creaciones y a su producto. El Estado propicia el acceso a la cultura y fomenta su desarrollo y difusión?')
+input('')
+input('a)Verdadero')
+input('b)Falso')
+input('')
+respuesta_3 = input("Respuesta:  ")
+if respuesta_3 != 'a' and respuesta_3 != 'A':
+  print('Respuesta Incorrecta')
+else:
+  print('Respuesta Correcta')
+input('')
+print("GRACIAS " + nombre + "!")
+input('')
+input("Pronto le notificaremos el siguiente nivel.")
+
+
+
+
+
